409509e376576a720d6f528c3e815c0f4df4b2ab
Boris Stanojevic <borisstanojevicfkkt@gmail.com>
2021-04-21 19:28:02 +0200

    Add files via upload

---
+++ validate_json_files/validate_json_files.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import json
+
+def validate_json_files(directory):
+    json_files = [ file for file in os.listdir(directory) if file.endswith("json") ]
+    
+    invalid_files = []
+    
+    for json_file in json_files:
+        with open (json_file, "r") as file:
+            content = file.read()
+            
+            try:
+                json.loads(content)
+            except ValueError as error:
+                print("Invalid json file: {0}. Error: {1}".format(json_file, error))
+                invalid_files.append(json_file)
+    
+    if len(invalid_files) > 0:
+        return False
+                
+if __name__ == "__main__":
+    validate_json_files(os.getcwd())
